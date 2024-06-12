d235ea9267c5a9e0fa0846bfd0aa59361a76fe31
romansyhinskiy <romansyhinskiy@gmail.com>
2024-06-12 15:35:23 +0300

    adding files

---
+++ components/index.ts
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import NavBar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarDetails from "./CarDetails";
+export {
+    Hero,
+    CustomButton,
+    NavBar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarDetails
+}
